2f304bd06a2354ee5b31c29726f257016e67c494
tjrohweder <tjrohweder@gmail.com>
2026-08-31 05:06:27 +0200

    Fixing outputs

--- terraform/env/dev/modules/outputs.tf
+++ terraform/env/dev/modules/outputs.tf
@@ -1,14 +1,11 @@
 output "cognito_user_pool_id" {
-  description = "ID do Cognito User Pool (necessário para o Ingress)"
   value       = aws_cognito_user_pool.dagster_auth.id
 }
 
 output "cognito_client_id" {
-  description = "ID do Client do Cognito (necessário para o Ingress)"
   value       = aws_cognito_user_pool_client.dagster_client.id
 }
 
 output "cognito_domain" {
-  description = "Domínio do Cognito"
   value       = aws_cognito_user_pool_domain.dagster_domain.domain
 }
